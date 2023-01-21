3427b900fe710834c03daddb5bb95b2600fa18e5
DDonghyeo <gsu9045000@gmail.com>
2023-01-22 00:30:49 +0900

    Feat: custom.html 디자인

--- static/js/result.js
+++ static/js/result.js
@@ -1,28 +1,61 @@
 //pie-chart
 $(window).ready(function(){
-    draw(80, '.pie-chart1', '#6188E8');
-    draw(50, '.pie-chart2', '#8b22ff');
-    draw(30, '.pie-chart3','#ff0');
+    draw(80, '.pie-chart1');
+    draw(50, '.pie-chart2');
+    draw(30, '.pie-chart3');
+    // painting('pie-chart1');
     typing();
+    
  });
  
- function draw(max, classname, colorname){
+ function draw(max, classname){
     var i=1;
      var func1 = setInterval(function(){
-       if(i<max){
-           color1(i,classname,colorname);
+       if(i<=max){
+           color1(i,classname);
            i++;
        } else{
          clearInterval(func1);
        }
+      //  if(i==100){
+      //   full(classname)
+      //  }
      },20);
  }
 
- function color1(i, classname,colorname){
+ 
+ function color1(i, classname){
     $(classname).css({
-         "background":"conic-gradient("+colorname+" 0% "+i+"%, #dfe0e5 "+i+"% 100%)"
+      // "background":"conic-gradient(#0066FF 0% "+i+"%, #E8EBF2 "+i+"% 100%)",
+      "background":"conic-gradient(rgba(0, 102, 255, 0.1) 0%, rgba(0, 102, 255, 1) "+i+"%, rgb(232, 235, 242) "+i+"%, rgb(232, 235, 242) 100%)"
     });
  }
+
+//  function full(classname){
+//   console.log(classname)
+//   $(classname).css({
+//     "background":"conic-gradient(rgba(0, 102, 255, 1))"
+//   });
+//   console.log("fin")
+// }
+
+//  function painting(classname){
+//   var i= 1;
+//    var func2 = setInterval(function(){
+//          paint(classname,i);
+//          i++;
+//          if(i==100){
+//           i=1;
+//         }
+//     },2000);
+// }
+
+// function paint(classname,i){
+//   console.log(i)
+//   $(classname).css({
+//     "background":"conic-gradient(rgba(0, 102, 255, 0.1) 0%, rgba(0, 102, 255, 1) "+i+"%, rgb(232, 235, 242) 80%, rgb(232, 235, 242) 100%)"
+//   });
+// }
 
  //숫자 카운트 애니메이션
  $('.count').each(function() { // .count-num에 각각 적용
